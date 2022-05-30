03fb673ffc533f154338c0aff453984d5a53d5ab
Justin Rinearson <justin@rinearson.io>
2022-05-29 20:20:03 -0700

    bg

--- Laptop-IS657-TeslaClone01/App.js
+++ Laptop-IS657-TeslaClone01/App.js
@@ -7,9 +7,7 @@ export default function App() {
     <View styles={styles.container}>
 
     <View styles={styles.carContainer}>
-    <ImageBackground 
-      source={require('laptop-is657-teslaclone01\assets\images\modelx.jpeg')} 
-      styles={styles.image}/>
+    
 
       <View style ={styles.titles}>
         <Text style={styles.title}>Model S</Text>
