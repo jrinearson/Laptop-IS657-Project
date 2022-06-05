4b91065c33597ed5af8e046b0b9ae54458967e4a
Justin Rinearson <justin@rinearson.io>
2022-06-04 20:39:29 -0700

    fixing buttons

--- Laptop-IS657-TeslaClone01/App.js
+++ Laptop-IS657-TeslaClone01/App.js
@@ -2,15 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from "react-native";
 import CarsList from './components/carlist';
+import Header from './components/header';
 
 export default function App() {
   return (
     
     <View styles={styles.container}>
-      
+      <Header/>
       <CarsList/>
       <StatusBar style="auto" />
-      
     </View>
   );
 }
