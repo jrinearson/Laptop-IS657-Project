c113a5e2c4ccdb3ca2c183e9e28395c3e643fea4
Justin Rinearson <justin@rinearson.io>
2022-06-18 13:23:38 -0700

    images added

--- IS657-UberEats/components/Categories/Categories.jsx
+++ IS657-UberEats/components/Categories/Categories.jsx
@@ -1,12 +1,43 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from "react-native";
+import React from "react";
+
+const items = [
+  {
+    image: require("../../assets/images/shopping-bag.png"),
+    text: "Pick-Up",
+  },
+  {
+    image: require("../../assets/images/bread.png"),
+    text: "Bakery Items",
+  },
+  {
+    image: require("../../assets/images/coffee.png"),
+    text: "Coffee & Tea",
+  },
+  {
+    image: require("../../assets/images/deals.png"),
+    text: "Deals",
+  },
+  {
+    image: require("../../assets/images/desserts.png"),
+    text: "Dessert",
+  },
+  {
+    image: require("../../assets/images/fast-food.png"),
+    text: "Fast Food",
+  },
+  {
+    image: require("../../assets/images/soft-drink.png"),
+    text: "Drinks",
+  },
+];
 
 function Categories() {
   return (
     <View>
       <Text>Categories</Text>
     </View>
-  )
-};
+  );
+}
 
 export default Categories;
