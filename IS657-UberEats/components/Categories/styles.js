e4aee660264fd5b3e845161c382d7aca00c0121c
Justin Rinearson <justin@rinearson.io>
2022-06-18 14:06:55 -0700

    finished styling categories container

--- IS657-UberEats/components/Categories/styles.js
+++ IS657-UberEats/components/Categories/styles.js
@@ -2,21 +2,31 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
     categoryImage: {
-        height: 40,
-        width: 50,
+        height: 30,
+        width: 40,
         resizeMode: 'contain',
-        //backgroundColor: 'white'
+        backgroundColor: '#eee',
     },
-
+    categoryImageContainer: {
+        height: 50,
+        width: 60,
+        resizeMode: 'contain',
+        backgroundColor: '#eee',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        marginBottom: 10,
+    },
+    
     categoryText: {
         fontSize: 13,
-        fontWeight: '800',
+        fontWeight: '500',
     },
 
     categoryContainer: {
         alignItems: 'center',
         marginHorizontal: 12,
-        marginVertical: 10
+        marginVertical: 10,
 
     },
 });
