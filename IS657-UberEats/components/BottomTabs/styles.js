3ea136076c2b91a40c03ae9e183fdc0aa5690a2b
Justin Rinearson <justin@rinearson.io>
2022-06-18 22:51:20 -0700

    home screen finished

--- IS657-UberEats/components/BottomTabs/styles.js
+++ IS657-UberEats/components/BottomTabs/styles.js
@@ -1,14 +1,29 @@
 import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     text: {
-        color: 'red',
-        
-        
+        fontSize: 11,
+        fontWeight: '600'
+         
     },
+
     tabContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginHorizontal: 15,
+        marginVertical: 10,
+        borderRadius: 30
+    },
+
+    buttonContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+
 
     },
 
 });
+
+
 
 export default styles;
