7464e4b546c1a4675997931553910de1e86a6df7
Justin Rinearson <justin@rinearson.io>
2022-06-23 22:38:20 -0700

    components done

--- IS657-UberEats/screens/OrderComplete/OrderComplete.jsx
+++ IS657-UberEats/screens/OrderComplete/OrderComplete.jsx
@@ -1,12 +1,28 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Image } from "react-native";
+import React, { useEffect } from "react";
 import styles from "./styles";
 import { useSelector } from "react-redux";
 import { SafeAreaView } from "react-native-safe-area-context";
 import LottieView from "lottie-react-native";
 import { ScrollView } from "react-native-gesture-handler";
+import firebase from "firebase";
+import { useState } from "react";
+import MenuItems from "../../components/RestaurantDetailComponents/MenuItems/MenuItems.jsx";
+
 
 export default function OrderComplete() {
+  const [lastOrder, setLastOrder] = useState({
+    items: [
+      {
+        title: "Bologna",
+        description: "With butter lettuce, tomato and sauce bechamel",
+        price: "$13.50",
+        image:
+          "https://www.modernhoney.com/wp-content/uploads/2019/08/Classic-Lasagna-14-scaled.jpg",
+      },
+    ],
+  });
+
   const { items, restaurantName } = useSelector(
     (state) => state.cartReducer.selectedItems
   );
@@ -20,29 +36,41 @@ export default function OrderComplete() {
     currency: "USD",
   });
 
+  useEffect(() => {
+    const db = firebase.firestore();
+    const unsubscribe = db
+      .collection("orders")
+      .orderBy("createdAt", "desc")
+      .limit(1)
+      .onSnapshot((snapshot) => {
+        snapshot.docs.map((doc) => {
+          setLastOrder(doc.data());
+        });
+      });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
     <SafeAreaView style={styles.page}>
-     <View
+      <View
         style={{
+        
           margin: 15,
           alignItems: "center",
           height: "100%",
+          
         }}
       >
-        <LottieView
-          style={{ height: 100, alignSelf: "center", marginBottom: 30 }}
-          source={require("../../assets/animations/check-mark.json")}
-          autoPlay
-          speed={0.5}
-          loop={false}
-        />
+        <Image source={require("../../assets/images/greencheckmark.jpg")} style={{height: 80, width: 80}} />
         <Text>
           Your order at {restaurantName} has been placed for {totalUSD}
         </Text>
-        
+
         <ScrollView>
+          <MenuItems foods={lastOrder.items} hideCheckbox={true} />
           <LottieView
-            style={{ height: 200, alignSelf: "center" }}
+            style={styles.cooking}
             source={require("../../assets/animations/cooking01.json")}
             autoPlay
             speed={0.5}
--- IS657-UberEats/screens/OrderComplete/styles.js
+++ IS657-UberEats/screens/OrderComplete/styles.js
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     page: {
         flex: 1,
-        backgroundColor: 'white'
+        backgroundColor: 'white',
     },
 
     checkbox: {
