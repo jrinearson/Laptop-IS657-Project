e9292711dfafc75300d11fb107df5dc06e4c36a9
Justin Rinearson <justin@rinearson.io>
2022-06-24 22:36:51 -0700

    help

--- UberEats/App.js
+++ UberEats/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import RootNavigation from "./Navigation.jsx";
 import { LogBox } from 'react-native';
 
+
 LogBox.ignoreLogs(['AsyncStorage has been extracted from react-native core and will be removed in a future release.']);
 
 
